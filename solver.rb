a0690069c3f7ddb32240b11a1a0e009fa270a276
Tushar <singhtushar348@gmail.com>
2022-10-06 17:58:22 +0530

    Write fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -13,5 +13,17 @@ class Solver
     string.reverse
   end
 
+  def fizzbuzz(n)
+    result = ''
+    (1..n).each do |i|
+      
+    result += 'fizzbuzz,' if(i%3 == 0 && i%5 == 0)
+    result += 'fizz,' if(i%3 == 0 && i%5 != 0)
+    result += 'buzz,' if(i%3 != 0 && i%5 == 0)
+    result += "#{i.to_s}," if(i%3 != 0 && i%5 != 0) 
+    end
+    result
+  end
+
 end
 
